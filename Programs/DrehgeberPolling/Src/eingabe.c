13e62f74b413e0a771c7ed801935fce9f92c6010
Esmat Rabaki <wcz654@haw-hamburg.de>
2025-11-07 17:19:09 +0100

    version2

--- Programs/DrehgeberPolling/Src/eingabe.c
+++ Programs/DrehgeberPolling/Src/eingabe.c
@@ -1,8 +1,43 @@
 
 #include "eingabe.h"
+#include "stm32f429xx.h"
+#include <stdbool.h>
 
-void input_signal() {
+00000000000001
+00000000000010
+
+#define IDR_MASK_PIN_0 (0x01U << (0))
+#define IDR_MASK_PIN_1 (0x01U << (1))
+
+
+bool givePinA(void)
+{
+    return (GPIOA->IDR & IDR_MASK_PIN_0) != 0;
+}
+bool givePinB(void)
+{
+    return (GPIOA->IDR & IDR_MASK_PIN_1) != 0;
+}
+
+
+void input_signal(void)
+{       
+     bool A = givePinA();
+     bool B = givePinB();
+
+     if (A && B) {
+
+
+
+
+
+
+    // Polling
+    
+}
+
+
 
 
     // Funktion zur Verarbeitung von Eingabesignalen
-}   
+   
